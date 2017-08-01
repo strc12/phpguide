18cacff55df03bf859988847fa836ce49355902d
strc12 <ric@oundleschool.org.uk>
2017-08-01 18:09:23 +0000

    first commit

---
+++ users.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Page title</title>
+    
+</head>
+<body>
+   <form action="adduser.php" method="post">
+  First name:<input type="text" name="forename"><br>
+  Last name:<input type="text" name="surname"><br>
+  Password:<input type="password" name="passwd"><br>
+  House:<input type="text" name="house"><br>
+  Year:<input type="text" name="year"><br>
+  <!--Creates a drop down list-->
+  Gender:<select name="gender">
+		<option value="M">Male</option>
+		<option value="F">Female</option>
+	</select>
+	<br>
+  <!--Next 3 lines create a radio button which we can use to select the user role-->
+  <input type="radio" name="role" value="Pupil" checked> Pupil<br>
+  <input type="radio" name="role" value="Teacher"> Teacher<br>
+  <input type="radio" name="role" value="Admin"> Admin<br>
+  <input type="submit" value="Add User">
+</form>
+<?php
+	include_once('connection.php');
+	$stmt = $conn->prepare("SELECT * FROM TblUser");
+	$stmt->execute();
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+		{
+			echo($row["Forename"].' '.$row["Surname"]."<br>");
+		}
+
+
+
+?>   
+</body>
+</html>
